4de9673dff6634adff186df7d34dad0f179a1771
KentaTashiro <62204430+KentaTashiro@users.noreply.github.com>
2022-01-09 18:52:06 +0900

    complete

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import axios from "axios";
 import { useState } from "react";
 import "./styles.css";
 import { Todo } from "./Todo";
+import { TodoType } from "./Types/todo";
+import { Text } from "./Text";
+import { UserProfile } from "./UserProfile";
+import { User } from "./Types/user";
 
-type TodoType = {
-  userId: number;
-  id: number;
-  title: string;
-  completed: boolean;
+const user: User = {
+  name: "田代"
+  // hobbies: ["netflix", "卓球"]
 };
 
 export default function App() {
@@ -21,9 +23,12 @@ export default function App() {
   };
   return (
     <div className="App">
+      <UserProfile user={user} />
+      <Text color="red" fontSize="18px" />
       <button onClick={onClickFetchData}>データ取得</button>
       {todos.map((todo) => (
         <Todo
+          key={todo.id}
           title={todo.title}
           userId={todo.userId}
           completed={todo.completed}
